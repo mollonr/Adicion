bfef94e5566c5441c72683143bc198714dc8f0fa
mollonr <mollonr@gmail.com>
2017-04-06 00:52:41 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,14 @@
+/* Rodrigo Mollon, Leg. 162821-5
+ * TP #1 _ Adicion
+ * V _ 20170330
+ */
+#include <iostream>
+int main(){
+int a; int b;
+std::cout << "Ingrese el primer numero de la suma: \n";
+std::cin >> a;
+std::cout << "Ingrese el segundo numero de la suma: \n";
+std::cin >> b;
+std::cout << "El resultado de la suma es: \n";
+std::cout << a << a + b;
+}
